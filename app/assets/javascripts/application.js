a710189edd554912a9b27c5d42600c3b68e8dfa4
Greg Hasseler <ghasseler@gmail.com>
2016-09-15 19:56:01 -0400

    Cleaned up assets. Added image assets.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery.dataTables.min.js
+//= require datatables
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( jquery.dataTables.min.css )
+#Rails.application.config.assets.precompile += %w( datatables.min.css )
+Rails.application.config.assets.precompile += %w( datatables.js )
+Rails.application.config.assets.precompile += %w( datatables.scss.erb )
+
 
