cd1d27a8b36997fa91d3bb0aed0ea36e03df08a7
Greg Hasseler <ghasseler@gmail.com>
2017-01-09 21:51:32 -0500

    Dropped migrations in favor of the updated schema.rb file.

--- db/migrate/20170104030122_create_underwriters.rb
+++
@@ -1,9 +0,0 @@
-class CreateUnderwriters < ActiveRecord::Migration[5.0]
-  def change
-    create_table :underwriters do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170107233025_add_abbreviation_to_underwriters.rb
+++
@@ -1,7 +0,0 @@
-class AddAbbreviationToUnderwriters < ActiveRecord::Migration[5.0]
-  def change
-    change_table :underwriters do |t|
-      t.string :abbreviation
-    end
-  end
-end
