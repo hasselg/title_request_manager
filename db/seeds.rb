2d16ada54901aa2a1b1ac8aefd95dc183d7fa404
Greg Hasseler <ghasseler@gmail.com>
2017-01-09 21:51:13 -0500

    Added some Underwriting companies to the seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,10 @@
 
 User.create! email: "test@example.com", password: 'password', password_confirmation: 'password', admin: true
 User.create! email: "test2@example.com", password: 'password', password_confirmation: 'password', admin: false
+
+Underwriter.create! name: "Fidelity National Title Insurance Company", abbreviation: "FN"
+Underwriter.create! name: "Lawyer's Title Insurance Company", abbreviation: "LT"
+Underwriter.create! name: "Ticor Title Insurance Company", abbreviation: "TT"
+Underwriter.create! name: "Westcor Title Insurance Company", abbreviation: "W"
+Underwriter.create! name: "Unknown", abbreviation: "Unknown"
+Underwriter.create! name: "Not Applicable", abbreviation: "N/A"
