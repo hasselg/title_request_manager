196685b1c0d8941f7d5a555bded1676ad4384879
Greg Hasseler <ghasseler@gmail.com>
2017-01-11 08:18:17 -0500

    Updated for Postgresql on development machine. Dropped unneeded column from Underwriters.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107233025) do
+ActiveRecord::Schema.define(version: 20170111020254) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "title_requests", force: :cascade do |t|
     t.text     "last_name"
@@ -67,11 +70,9 @@ ActiveRecord::Schema.define(version: 20170107233025) do
 
   create_table "underwriters", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "underwriter_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "abbreviation"
-    t.index ["underwriter_id"], name: "index_underwriters_on_underwriter_id"
   end
 
   create_table "underwritings", force: :cascade do |t|
@@ -79,8 +80,8 @@ ActiveRecord::Schema.define(version: 20170107233025) do
     t.integer  "title_request_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.index ["title_request_id"], name: "index_underwritings_on_title_request_id"
-    t.index ["underwriter_id"], name: "index_underwritings_on_underwriter_id"
+    t.index ["title_request_id"], name: "index_underwritings_on_title_request_id", using: :btree
+    t.index ["underwriter_id"], name: "index_underwritings_on_underwriter_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -95,7 +96,7 @@ ActiveRecord::Schema.define(version: 20170107233025) do
     t.integer  "failed_attempts",     default: 0,     null: false
     t.datetime "locked_at"
     t.boolean  "admin",               default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
